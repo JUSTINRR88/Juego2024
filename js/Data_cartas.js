10b570fc679cfe92db03319bff1be52371fff9b7
JUSTINRR88 <126489355+JUSTINRR88@users.noreply.github.com>
2024-06-05 10:13:52 -0600

    Add files via upload

---
+++ js/Data_cartas.js
@@ -0,0 +1,26 @@
+// CARTAS
+export let lista_cartas_nivel = [
+    ["🚀","🛰️"],
+    ["🚀","🛰️","👽","🛸"],
+    ["🚀","🛰️","👽","🛸","🪂","🌌"],
+    ["🚀","🛰️","👽","🛸","🪂","🌌","⭐","🌑"],
+    ["🚀","🛰️","👽","🛸","🪂","🌌","⭐","🌑","🔭","👩‍🚀"],
+    ["🚀","🛰️","👽","🛸","🪂","🌌","⭐","🌑","🔭","👩‍🚀","☄️","👾"]
+];
+
+export let cards1 = ["🚀","🛰️"];
+export let cards2 = ["🚀","🛰️"];
+
+// Función para crear aleatoriedad
+function ordenarAleatorio(a, b) {
+    return Math.random() - 0.5;
+}
+
+// Unificar las listas
+let todas_las_tarjetas = cards1.concat(cards2);
+
+
+// Organiza de forma aleatoria la lista todas_las_cartas
+todas_las_tarjetas.sort(ordenarAleatorio);
+
+export let lista_random = todas_las_tarjetas;
